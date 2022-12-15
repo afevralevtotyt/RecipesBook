3ffb4a31a349cc8e2a982d4931cf6034d5b8e17f
afevralevtotyt <afevraleff@ya.ru>
2022-12-15 21:39:47 +0200

    Initial commit

---
+++ src/test/java/me/fevralev/recipesbook/RecipesBookApplicationTests.java
@@ -0,0 +1,13 @@
+package me.fevralev.recipesbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipesBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
